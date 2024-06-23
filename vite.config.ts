8a4a1ff66905cac10569caf65c13d70c787eab88
Julius Friedman <jfriedman@cantaloupe.com>
2024-06-23 05:46:16 -0400

    switch to esm.sh

--- vite.config.ts
+++ vite.config.ts
@@ -26,11 +26,11 @@ export default defineConfig({
                 paths: {
                     pinia: 'https://cdn.jsdelivr.net/npm/pinia@^2.1.7/+esm',
                     '@iconify/vue': 'https://cdn.jsdelivr.net/npm/@iconify/vue@^4.1.2/+esm',
-                    'radix-vue': 'https://cdn.jsdelivr.net/npm/radix-vue@^1.8.4/+esm',
+                    'radix-vue': 'https://esm.sh/radix-vue@1.8.4?deps=vue@3.4.30',
                     '@radix-ui/colors': 'https://cdn.jsdelivr.net/npm/@radix-ui/colors@^3.0.0/+esm',
                     'tailwindcss': 'https://cdn.jsdelivr.net/npm/tailwind@^3.4.4/+esm',
-                    vue: 'https://cdn.jsdelivr.net/npm/vue@^3.4.29/+esm',
-                    'vue-router': 'https://cdn.jsdelivr.net/npm/vue-router@^4.4.0/+esm'
+                    vue: 'https://esm.sh/vue@3.4.30',
+                    'vue-router': 'https://esm.sh/vue-router@4.4.0?deps=vue@3.4.30',
                 }
             }
         }
@@ -41,11 +41,11 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url)),
             pinia: 'https://cdn.jsdelivr.net/npm/pinia@^2.1.7/+esm',
             '@iconify/vue': 'https://cdn.jsdelivr.net/npm/@iconify/vue@^4.1.2/+esm',
-            'radix-vue': 'https://cdn.jsdelivr.net/npm/radix-vue@^1.8.4/+esm',
+            'radix-vue': 'https://esm.sh/radix-vue@1.8.4?deps=vue@3.4.30',
             '@radix-ui/colors': 'https://cdn.jsdelivr.net/npm/@radix-ui/colors@^3.0.0/+esm',
             'tailwindcss': 'https://cdn.jsdelivr.net/npm/tailwind@^3.4.4/+esm',
-            vue: 'https://cdn.jsdelivr.net/npm/vue@^3.4.29/+esm',
-            'vue-router': 'https://cdn.jsdelivr.net/npm/vue-router@^4.4.0/+esm'
+            vue: 'https://esm.sh/vue@3.4.30',
+            'vue-router': 'https://esm.sh/vue-router@4.4.0?deps=vue@3.4.30',
         }
     }
 });
